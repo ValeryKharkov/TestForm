b0ad29037520811e5845072a759c307962f0b560
valery <kharkov.valery@gmail.com>
2025-03-21 23:41:53 +0300

    Тест окна авторизации: Логин

--- src/test/java/TestForm.java
+++ src/test/java/TestForm.java
@@ -1,11 +1,24 @@
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestForm {
     @Test
     public void testLogin() {
         WebDriver driver = new ChromeDriver();
-        driver.get("file:/home/valery/IdeaProjects/TestForm/src/main/resources/qa-test.html");
+        // TODO Убрать хардкод
+        driver.get("file:/home/valery/IdeaProjects/TestForm/src/test/resources/qa-test.html");
+
+        WebElement emailField = driver.findElement(By.name("loginEmail"));
+        WebElement passwordField = driver.findElement(By.id("loginPassword"));
+        WebElement loginButton = driver.findElement(By.id("authButton"));
+
+        emailField.sendKeys("test@protei.ru");
+        passwordField.sendKeys("test");
+        loginButton.click();
+
+        driver.quit();
     }
 }
