6ec1decf9894f5651a5ee5347e627a44af9b91ac
valery <kharkov.valery@gmail.com>
2025-03-23 00:01:30 +0300

    Написание позитивных тестов (Страница авторизации)

--- src/test/java/TestForm.java
+++ src/test/java/TestForm.java
@@ -4,14 +4,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class TestForm {
+
     @Test
     public void testLogin() {
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
         WebDriver driver = new ChromeDriver();
         // TODO Убрать хардкод
         driver.get("file:/home/valery/IdeaProjects/TestForm/src/test/resources/qa-test.html");
+        driver.manage().window().maximize();
 
-        WebElement emailField = driver.findElement(By.name("loginEmail"));
+//        WebElement emailField = driver.findElement(By.xpath("//input[@id='loginEmail']"));
+        WebElement emailField = driver.findElement(By.id("loginEmail"));
         WebElement passwordField = driver.findElement(By.id("loginPassword"));
         WebElement loginButton = driver.findElement(By.id("authButton"));
 
