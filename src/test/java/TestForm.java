042d5f3ec1fa62642225b4618269ae5c013f0154
valery <kharkov.valery@gmail.com>
2025-03-23 22:13:07 +0300

    Оптимизация проверок

--- src/test/java/TestForm.java
+++ src/test/java/TestForm.java
@@ -3,21 +3,30 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
 
 public class TestForm {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeClass
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
 
     @Test
-    public void testLogin() {
-        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
-        WebDriver driver = new ChromeDriver();
+    public void testLoginForm() {
         // TODO Убрать хардкод
         driver.get("file:/home/valery/IdeaProjects/TestForm/src/test/resources/qa-test.html");
         driver.manage().window().maximize();
 
-        // Страница авторизации
-//        WebElement emailField = driver.findElement(By.xpath("//input[@id='loginEmail']"));
         WebElement emailField = driver.findElement(By.id("loginEmail"));
         WebElement passwordField = driver.findElement(By.id("loginPassword"));
         WebElement loginButton = driver.findElement(By.id("authButton"));
@@ -26,41 +35,47 @@ public class TestForm {
         passwordField.sendKeys("test");
         loginButton.click();
 
-        WebElement inputsPage = driver.findElement(By.id("inputsPage"));
-        Assert.assertTrue(inputsPage.isDisplayed());
+        // Проверка успешной авторизации
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inputsPage")));
+        Assert.assertTrue(driver.findElement(By.id("inputsPage")).isDisplayed(), "Авторизация не удалась");
+    }
 
-        // Страница анкеты. Ввод и добавление данных
-        WebElement dataEmail = driver.findElement(By.id("dataEmail"));
-        WebElement dataName = driver.findElement(By.id("dataName"));
-        WebElement dataGender = driver.findElement(By.id("dataGender"));
-        WebElement dataCheck11 = driver.findElement(By.id("dataCheck11"));
-        WebElement dataCheck12 = driver.findElement(By.id("dataCheck12"));
-        WebElement dataSelect21 = driver.findElement(By.id("dataSelect21"));
-        WebElement dataSelect22 = driver.findElement(By.id("dataSelect22"));
-        WebElement dataSelect23 = driver.findElement(By.id("dataSelect23"));
-        WebElement dataSendButton = driver.findElement(By.id("dataSend"));
+    @Test
+    public void testDataForm() {
+            WebElement dataEmail = driver.findElement(By.id("dataEmail"));
+            WebElement dataName = driver.findElement(By.id("dataName"));
+            WebElement dataGender = driver.findElement(By.id("dataGender"));
+            WebElement dataCheck11 = driver.findElement(By.id("dataCheck11"));
+            WebElement dataCheck12 = driver.findElement(By.id("dataCheck12"));
+            WebElement dataSelect21 = driver.findElement(By.id("dataSelect21"));
+            WebElement dataSelect22 = driver.findElement(By.id("dataSelect22"));
+            WebElement dataSelect23 = driver.findElement(By.id("dataSelect23"));
+            WebElement dataSendButton = driver.findElement(By.id("dataSend"));
 
-        dataEmail.sendKeys("user@example.com");
-        dataName.sendKeys("User");
-        // TODO реализовать выбор гендера из выпадающего списка
-        dataGender.click();
-        dataGender.sendKeys("Женский");
-        dataGender.click();
-        dataGender.sendKeys("Мужской");
-        dataCheck11.click();
-        dataCheck12.click();
-        dataSelect21.click();
-        dataSelect22.click();
-        dataSelect23.click();
-        dataSendButton.click();
+            dataEmail.sendKeys("user@example.com");
+            dataName.sendKeys("User");
+            // TODO реализовать выбор гендера из выпадающего списка, а не подстановкой значения
+            dataGender.click();
+            dataGender.sendKeys("Женский");
+            dataGender.click();
+            dataGender.sendKeys("Мужской");
+            dataCheck11.click();
+            dataCheck12.click();
+            dataSelect21.click();
+            dataSelect22.click();
+            dataSelect23.click();
+            dataSendButton.click();
 
-        // TODO Добавить xpath в enum файл
-        WebElement popup = driver.findElement(By.xpath("//div[@class='uk-margin uk-modal-content' and contains(text(), 'Данные добавлены.')]"));
-        WebElement popupOkButton = driver.findElement(By.xpath("//button[contains(text(), 'Ok')]"));
-        popup.getText().equals("Данные добавлены.");
-        popupOkButton.click();
+            // TODO Добавить xpath в enum файл
+            WebElement popup = driver.findElement(By.xpath("//div[@class='uk-margin uk-modal-content' and contains(text(), 'Данные добавлены.')]"));
+            WebElement popupOkButton = driver.findElement(By.xpath("//button[contains(text(), 'Ok')]"));
+            popup.getText().equals("Данные добавлены.");
+            popupOkButton.click();
+    }
 
-        // Страница анкеты. Проверка таблицы с добавленными данными
+    @Test
+    public void checkTable() {
+
         WebElement tableRow = driver.findElement(By.xpath("//table[@id='dataTable']/tbody/tr"));
         Assert.assertTrue(tableRow.getText().contains("user@example.com"));
         Assert.assertTrue(tableRow.getText().contains("User"));
@@ -68,7 +83,10 @@ public class TestForm {
         Assert.assertTrue(tableRow.getText().contains("1.1"));
         Assert.assertTrue(tableRow.getText().contains("1.2"));
         Assert.assertTrue(tableRow.getText().contains("2.3"));
+    }
 
+    @AfterClass
+    public void quitBrowser() {
         driver.quit();
     }
 }
